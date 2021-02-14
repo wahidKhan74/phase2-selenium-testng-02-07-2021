8bd38b6c93cbd083425d6a026139cb501bd6b4f4
Waheed Khan <wahid.khan74@gmail.com>
2021-02-14 16:31:40 +0000

    add to cart test with amazon

---
+++ src/test/java/com/ecom/selenium/external/browser/popup/AddToCartTest.java
@@ -0,0 +1,67 @@
+package com.ecom.selenium.external.browser.popup;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddToCartTest {
+
+	WebDriver driver;
+	final String siteURL = "https://www.amazon.in/";
+	final String driverPATH = "/home/wahidkhan74gmai/eclipse-workspace/amazontests/driver/geckodriver";
+
+	@BeforeMethod
+	public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver", driverPATH);
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.get(siteURL);
+	}
+
+	@AfterMethod
+	public void afterTest() {
+//		driver.close();
+	}
+
+	@Test
+	public void addToCartTest() throws InterruptedException {
+		
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 11 pro max");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[2]/div[2]/div/span/div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a"))
+				.click();
+		
+		String mainWindow = driver.getWindowHandle();
+		
+		ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+		
+		driver.switchTo().window(tabs.get(1));
+		
+		driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+		    
+	//		Set<String> childWs = driver.getWindowHandles();
+	//		Iterator<String> windowIterator = childWs.iterator();
+	//		
+	//		while(windowIterator.hasNext()) {
+	//			String childWindow = windowIterator.next();
+	//			if(mainWindow.equalsIgnoreCase(childWindow)) {
+	//				Thread.sleep(2000);
+	//				driver.switchTo().window(childWindow);
+	//				Thread.sleep(2000);
+	//				driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+	//				driver.close();
+	//			}
+	//		}	
+		
+	}
+}
